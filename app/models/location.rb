30f3495a05bc0899cf05d9537e20227be0989af9
Diana Baiadori <diana.baiadori@gmail.com>
2021-11-16 15:04:48 +0100

    fix: fixed the locations model in accordance to the photos

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ApplicationRecord
   has_many_attached :photo
-  belongs_to :user
-  has_many :bookings
+  # belongs_to :user
+  # has_many :bookings
   validates :name, presence: true, uniqueness: true, length: { minimum: 6 }
   validates :property_type, presence: true
   validates :location_address, presence: true
