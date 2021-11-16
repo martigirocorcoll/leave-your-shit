d6991480a6810df1a3bebe709b8ec40815a3eea5
Diana Baiadori <diana.baiadori@gmail.com>
2021-11-16 13:07:05 +0100

    added active storage to the photo. can only add a single photo for now

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,10 @@
 class Location < ApplicationRecord
+  has_one_attached :photo
   #belongs_to :user
-  validates :name, presence: true, uniqueness: true, length: { minimum: 6 }
-  validates :property_type, presence: true
-  validates :location_address, presence: true
-  validates :description, presence: true, length: { minimum: 20 }
-  validates :availability, exclusion: [nil]
-  validates :price, presence: true, numericality: true
+  # validates :name, presence: true, uniqueness: true, length: { minimum: 6 }
+  # validates :property_type, presence: true
+  # validates :location_address, presence: true
+  # validates :description, presence: true, length: { minimum: 20 }
+  # validates :availability, exclusion: [nil]
+  # validates :price, presence: true, numericality: true
 end
