74171eb4112f10d10a141e3da3920197c0dc9f96
Diana Baiadori <diana.baiadori@gmail.com>
2021-11-16 14:54:52 +0100

    updated the location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,10 +1,11 @@
 class Location < ApplicationRecord
   has_many_attached :photo
-  #belongs_to :user
-  # validates :name, presence: true, uniqueness: true, length: { minimum: 6 }
-  # validates :property_type, presence: true
-  # validates :location_address, presence: true
-  # validates :description, presence: true, length: { minimum: 20 }
-  # validates :availability, exclusion: [nil]
-  # validates :price, presence: true, numericality: true
+  belongs_to :user
+  has_many :bookings
+  validates :name, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates :property_type, presence: true
+  validates :location_address, presence: true
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :availability, exclusion: [nil]
+  validates :price, presence: true, numericality: true
 end
