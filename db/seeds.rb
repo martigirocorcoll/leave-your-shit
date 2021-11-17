ee8a6ec8c802ec2bd75ec313c2ceea541f99aa75
Diana Baiadori <diana.baiadori@gmail.com>
2021-11-17 13:07:34 +0100

    finished implementing the seeds and adjusted the show page to display the right image

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,13 @@ require "open-uri"
 
 puts "cleaning up database"
 Location.destroy_all
+User.destroy_all
 puts "Database is clean"
 
 puts "Creating locations"
 
 def random_address
+  # Samples a random, real address from Barcelona, Porto or Sevilla
   ["Passeig de Gracia, Barcelona", "Passeig de Sant Joan, Barcelona", "Passeig del Born, Barcelona",
    "Carrer del Consell de Cent, Barcelona", "Carrer dels Tallers, Barcelona", "Carrer del Blai, Barcelona",
    "Rua de Santa Catarina, Porto", "Rua de Boavista, Porto", "Rua do Campo Alegre, Porto",
@@ -17,35 +19,44 @@ def random_address
 end
 
 def random_property
+  # Samples a random property type
   %w[garage spare-room basement container shed attic warehouse office].sample
 end
 
+def concordance(adjective)
+  # Ensures there is concordance in the name of the location
+  if %w[a e i o u].include?(adjective[0])
+    "An #{adjective}"
+  else
+    "A #{adjective}"
+  end
+end
 
 4.times do
   User.create!(
     email: Faker::Internet.email,
     password: "a766868sf",
-    name:"adfgf",
-    phone_number: "adfgf",
-    seller_type: "adfgf"
+    name: Faker::FunnyName.name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    seller_type: ["professional", "personal"].sample
   )
 end
 
+
+
 10.times do
-
-  file = URI.open('https://source.unsplash.com/1600x900/?storage')
-
-
+  adjective = Faker::Adjective.positive
   location = Location.new(
     availability: true,
     location_address: random_address,
     price: Faker::Number.between(from: 1, to: 10),
-    name: "A #{Faker::Adjective.positive} storage space by #{Faker::FunnyName.name}",
     description: "#{Faker::Company.catch_phrase}: #{Faker::Company.bs}",
     property_type: random_property,
-    user_id: User.all.sample.id,
+    user_id: User.all.sample.id
   )
-  location.name = "A #{Faker::Adjective.positive} #{location.property_type} by #{Faker::FunnyName.name}"
+  keyword_property = location.property_type.gsub("-", " ")
+  file = URI.open("https://source.unsplash.com/1600x900/?storage,#{keyword_property}")
+  location.name = "#{concordance(adjective)} #{location.property_type} by #{location.user.name}"
   location.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
   location.save!
   p "Created #{location.name}"
