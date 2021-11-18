9a140fcf662c0db09b65d66235ffaf405c530081
lucypinker <lucypinker22@gmail.com>
2021-11-18 15:06:55 +0100

    booking confirmation front and back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_103401) do
+ActiveRecord::Schema.define(version: 2021_11_18_102236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_103401) do
     t.integer "agreed_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
     t.index ["location_id"], name: "index_bookings_on_location_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
