4000435d1dc8c16dd5afe8c0518cd5e9e8cec3dc
lucypinker <lucypinker22@gmail.com>
2021-11-19 17:03:21 +0100

    dashboard updates2

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,7 @@ class DashboardsController < ApplicationController
   def show
     @bookings = current_user.bookings
     @requests = current_user.requests.pending
+    @locations = current_user.locations
   end
 
 
